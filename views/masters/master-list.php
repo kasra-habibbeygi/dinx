254a058405882520544ce731121878177d78e0eb
nadie-najm <nadi.codes@gmail.com>
2021-06-06 15:47:03 +0430

    master card first commit

---
+++ views/masters/master-list.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/general/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/general/general-css.css">
+    <link rel="stylesheet" href="../../assets/css/masters/master-list.css">
+    <title> دینکس | لیست اساتید</title>
+</head>
+
+<body>
+
+    <?php require_once '../template/navbar.php'?>
+    <main class="main_ms">
+        <?php require_once '../template/modals.php'?>
+
+        <section>
+            <p class="title_sec">لیست اساتید دینکس</p>
+            <div class="masters">
+                <a href="#" class="master_card">
+                    <img src="../../assets/img/VS02.PNG" alt="حمید آذر مکان" title="حمید آذر مکان" class="card_img"> 
+                    <div class="card_content">  
+                    <img src="../../assets/img/CB00.svg" class="card_bg">
+                    <p>حمید آذر مکان</p>
+                    </div>
+                    
+                </a>
+            </div>
+        </section>
+
+    </main>
+
+    <?php require_once '../template/footer.php'?>
+
+    <script src="../../assets/js/general/jquery.js"></script>
+    <script src="../../assets/js/general/bootstrap.js"></script>
+    <script src="../../assets/js/script/navbar.js"></script>
+    <script src="../../assets/js/script/modals.js"></script>
+
+</body>
+
+</html>
