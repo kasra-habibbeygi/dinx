ea639c73975b0951355798f2c2baca894b95db6e
nadie-najm <nadi.codes@gmail.com>
2021-06-10 21:42:19 +0430

    master profile

---
+++ views/masters/master-profile.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/general/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/general/fontAwsome.css">
+    <link rel="stylesheet" href="../../assets/css/general/general-css.css">
+    <link rel="stylesheet" href="../../assets/css/masters/master-profile.css">
+    <title> دینکس | پروفایل اساتید</title>
+</head>
+
+<body>
+
+    <?php require_once '../template/navbar.php'?>
+    <main class="main_mp">
+        <?php require_once '../template/modals.php'?>
+        <section class="pr_h">
+            <div class="pr_img"></div>
+            <div class="content_pr">
+                <p>ابوالفضل خداشناس </p>
+                <div class="socail">
+                    <a href="#"><i class="fas fa-phone-alt icon"></i></a>
+                    <a href="#"><i class="far fa-at icon"></i></a>
+                    <a href="#"><i class="fab fa-whatsapp icon"></i></a>
+                    <a href="#"><i class="fab fa-telegram-plane icon icon"></i></a>
+                </div>
+            </div>
+        </section>
+        <div class="hr"></div>
+        <section class="pr_ab">
+            <div class="content_ab">
+                <h2>درباره من</h2>
+                <p class="exp_ab">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                    چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی
+                    مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه
+                    درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری
+                    را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
+
+                </p>
+                <p class="exp_ab">
+                    در این
+                    صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد
+                    وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی
+                    اساسا مورد استفاده قرار گیرد.
+                </p>
+            </div>
+            <div class="content_specialty">
+                <h2>تخصص ها</h2>
+                <div class="sp_b">
+                    <p class="specialty">طراح و مجری پیمانکار در صنعت ساختمان</p>
+                    <p class="specialty">موسس و مدیر شرکت مهندسی آوندگستر طراحان شمال</p>
+                    <p class="specialty">موسس و مدیر آموزشگاه ایده نو</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="pr_tc">
+            <h3> دوره های استاد ابوالفضل خداشناس</h3>
+
+            <div class="card_parent">
+                <a href="#" class="course_card">
+                    <div class="card_img" style="background-image: url(../../assets/img/CI00.jpg);"></div>
+                    <div class="card_content">
+                        <p class="card_title">اموزش زبان تخمی دات جی اس به همراه پروژه در تخمستان</p>
+                        <div class="card_detail">
+                            <div><i class="fas fa-user-alt"></i>کسری حبیب بیگی</div>
+                            <div><i class="fas fa-eye"></i>۱۰۲۳</div>
+                        </div>
+                        <img src="../../assets/img/CB00.svg" class="card_bg"></img>
+                    </div>
+                    <div class="card_footer">
+                        <p><i class="far fa-clock"></i>۱۵ : ۰۰</p>
+                        <p>۱۰۰.۰۰۰ <span>تومان</span></p>
+                    </div>
+                </a>
+                <a href="#" class="course_card">
+                    <div class="card_img" style="background-image: url(../../assets/img/CI01.jpg);"></div>
+                    <div class="card_content">
+                        <p class="card_title">PHP آموزش هک و امنیت</p>
+                        <div class="card_detail">
+                            <div><i class="fas fa-user-alt"></i>کسری حبیب بیگی</div>
+                            <div><i class="fas fa-eye"></i>۱۰۲۳</div>
+                        </div>
+                        <img src="../../assets/img/CB00.svg" class="card_bg"></img>
+                    </div>
+                    <div class="card_footer">
+                        <p><i class="far fa-clock"></i>۱۵ : ۰۰</p>
+                        <p>۱۰۰.۰۰۰ <span>تومان</span></p>
+                    </div>
+                </a>
+                <a href="#" class="course_card">
+                    <div class="card_img" style="background-image: url(../../assets/img/CI02.jpg);"></div>
+                    <div class="card_content">
+                        <p class="card_title">PHP آموزش هک و امنیت</p>
+                        <div class="card_detail">
+                            <div><i class="fas fa-user-alt"></i>کسری حبیب بیگی</div>
+                            <div><i class="fas fa-eye"></i>۱۰۲۳</div>
+                        </div>
+                        <img src="../../assets/img/CB00.svg" class="card_bg"></img>
+                    </div>
+                    <div class="card_footer">
+                        <p><i class="far fa-clock"></i>۱۵ : ۰۰</p>
+                        <p>۱۰۰.۰۰۰ <span>تومان</span></p>
+                    </div>
+                </a>
+            </div>
+        </section>
+
+    </main>
+
+    <?php require_once '../template/footer.php'?>
+
+    <script src="../../assets/js/general/jquery.js"></script>
+    <script src="../../assets/js/script/navbar.js"></script>
+    <script src="../../assets/js/script/modals.js"></script>
+
+</body>
+
+</html>
