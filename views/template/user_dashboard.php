1eed4f3641b544614be41bf0f54498abca07b13c
meganoob <kasrahabibbeygi@gmail.com>
2021-06-10 11:12:33 +0430

    creat user dashboard

---
+++ views/template/user_dashboard.php
@@ -0,0 +1,70 @@
+<aside class="user_dashboard_field">
+    <div class="UD_main_field">
+        <div class="user_avatar" style="background-image: url(../../assets/img/MI00.jpg);"></div>
+        <p>کسری حبیب بیگی</p>
+
+        <ul class="dashboard_item_field">
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    داشبورد
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    سوابق خرید
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    وضعیت دروس
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    فروم های من
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    تکلیف های ارسالی
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    تیکت ها
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    گواهی ها
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    ویرایش پروفایل
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    تعویض رمز عبور
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-circle"></i>
+                    خروج
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</aside>
---
+++ views/user_profile/dashboard.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/general/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/general/fontAwsome.css">
+    <link rel="stylesheet" href="../../assets/css/general/general-css.css">
+    <link rel="stylesheet" href="../../assets/css/user-profile/dashboard.css">
+    <title>دینکس | بترین سایت آموزشی</title>
+</head>
+
+<body>
+
+    <?php require_once '../template/navbar.php'?>
+
+    <main class="user_dashboard">
+        <?php require_once '../template/modals.php'?>
+
+        <section class="profile_temp">
+            <?php require_once '../template/user_dashboard.php'?>
+
+            <div class="profile_content">
+
+                <div class="dashboard_card">
+                    <div class="C_field">
+                        <img class="SPI" src="../../assets/img/DC02.svg" alt="">
+                        <div>
+                            <span>۵</span>
+                            <p>تعداد دوره ها</p>
+                        </div>
+                    </div>
+                    <div class="C_field">
+                        <img src="../../assets/img/DC01.svg" alt="">
+                        <div>
+                            <span>۴</span>
+                            <p>تعداد تخصص ها</p>
+                        </div>
+                    </div>
+                    <div class="C_field">
+                        <img src="../../assets/img/DC00.svg" alt="">
+                        <div>
+                            <span>۲</span>
+                            <p>تعداد گواهی ها</p>
+                        </div>
+                    </div>
+                    <div class="C_field">
+                        <img src="../../assets/img/DC03.svg" alt="">
+                        <div>
+                            <span>۴۳</span>
+                            <p>تکلیف های ارسالی</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="content_field">
+                    <header class="main_sec_header">
+                        <h3>اطلاعات پروفایل کاربری</h3>
+                    </header>
+
+                    <div class="general_content user_info">
+                        <div class="MF">
+                            <p>نام و نام خانوادگی : <span> کسری حبیب بیگی</span></p>
+                        </div>
+                        <div class="MF">
+                            <p>تاریخ تولد : <span> ۱۳۷۷.۱۰.۵</span></p>
+                        </div>
+                        <div class="MF">
+                            <p>مقطع تحصیلی <span> کارشناسی</span></p>
+                        </div>
+                        <div class="MF">
+                            <p>موبایل : <span> ۰۹۰۲۱۴۱۴۸۶۶</span></p>
+                        </div>
+                        <div class="MF">
+                            <p>دشته تحصیلی : <span> مهندسی نرم افزار</span></p>
+                        </div>
+                        <div class="MF">
+                            <p>تلفن ثابت : <span> ۰۲۱-۴۴۳۰۳۷۸۵</span></p>
+                        </div>
+                        <div class="MF">
+                            <p>آدرس : <span> تهران - شهران -خیابان طوقانی - خیایبان یاس - پلاک 28 </span></p>
+                        </div>
+                        <div class="MF">
+                            <div class="favorit"> <span class="title">علاقه مندی ها :</span> <span>بازی های
+                                    کامپیوتری</span><span>برنامه نویسی</span><span>بازی های کامپیوتری</span><span>برنامه
+                                    نویسی</span> <span>طراحی وب سایت</span><span>خواندن کتاب </span></div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="content_field">
+                    <header class="main_sec_header">
+                        <h3>اطلاعات پروفایل کاربری</h3>
+                    </header>
+
+                    <div class="general_content ticket">
+                        <div class="table_body">
+                            <div class="T_header">
+                                <div class="T_title">
+                                    ردیف
+                                </div>
+                                <div class="T_title">
+                                    شماره تیکت
+                                </div>
+                                <div class="T_title">
+                                    عنوان
+                                </div>
+                                <div class="T_title">
+                                    تاریخ ثبت
+                                </div>
+                                <div class="T_title">
+                                    وضعیت
+                                </div>
+                                <div class="T_title">
+                                    جزئیات
+                                </div>
+                            </div>
+                            <div class="T_body">
+                                <div class="T_data">
+                                    ۱
+                                </div>
+                                <div class="T_data">
+                                    ۱۶۵۷۹۰۰
+                                </div>
+                                <div class="T_data">
+                                    <div class="text_limited">
+                                        خرید نا موفق
+                                    </div>
+                                </div>
+                                <div class="T_data">
+                                    ۱۴۰۰.۱۰.۳۰
+                                </div>
+                                <div class="T_data">
+                                    <div class="green_situation">
+                                    
+                                    </div>
+                                </div>
+                                <div class="T_data">
+                                    ۱
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+
+    <?php require_once '../template/footer.php'?>
+
+    <script src="../../assets/js/general/jquery.js"></script>
+    <script src="../../assets/js/general/bootstrap.js"></script>
+    <script src="../../assets/js/script/navbar.js"></script>
+    <script src="../../assets/js/script/modals.js"></script>
+    <script src="../../assets/js/script/user_dashboard.js"></script>
+
+</body>
+
+</html>
