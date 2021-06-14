19d8d8cc82176ae0d4862216c72173eb65ee9ad7
nadie-najm <nadi.codes@gmail.com>
2021-06-14 17:43:57 +0430

    article ;ist

---
+++ views/articles/article-list.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/general/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/general/fontAwsome.css">
+    <link rel="stylesheet" href="../../assets/css/general/general-css.css">
+    <link rel="stylesheet" href="../../assets/css/articles/artcile-list.css">
+    <title> دینکس | لیست مقالات</title>
+</head>
+
+<body>
+
+    <?php require_once '../template/navbar.php'?>
+    <main class="main_arl">
+        <?php require_once '../template/modals.php'?>
+        <div class="card_parent">
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI00.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI01.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI02.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI00.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI01.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI02.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI00.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI01.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI02.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+            <a href="#" class="course_card">
+                <div class="card_img" style="background-image: url(../../assets/img/CI00.jpg);"></div>
+                <div class="card_content">
+                    <p class="card_title">نرم افزار طراحی معماری</p>
+                    <p class="card_short_exp">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                        چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                    </p>
+                    <div class="card_detail">
+                        <div><i class="far fa-clock"></i>کسری حبیب بیگی</div>
+                        <div>1400.10.10</div>
+                    </div>
+
+                </div>
+
+            </a>
+        </div>
+
+    </main>
+
+    <?php require_once '../template/footer.php'?>
+
+    <script src="../../assets/js/general/jquery.js"></script>
+    <script src="../../assets/js/script/navbar.js"></script>
+    <script src="../../assets/js/script/modals.js"></script>
+
+</body>
+
+</html>
